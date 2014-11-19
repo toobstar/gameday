ef07758777bd78a0131af33fc26737d433a5eef5
Toby <toby@skaffold.com>
2014-11-19 13:26:08 +1100

    first commit

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ module.exports = {
 	// the database url to connect
 	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
 	//url : 'mongodb://admin:_vx3Cb-zv6VC@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/gd'
-	url : 'mongodb://admin:_vx3Cb-zv6VC@process.env.OPENSHIFT_MONGODB_DB_HOST:process.env.OPENSHIFT_MONGODB_DB_PORT/gd'																				
+	//url : 'mongodb://admin:_vx3Cb-zv6VC@process.env.OPENSHIFT_MONGODB_DB_HOST:process.env.OPENSHIFT_MONGODB_DB_PORT/gd'																				
+	url : 'mongodb://admin:_vx3Cb-zv6VC@127.9.117.1:process.env.OPENSHIFT_MONGODB_DB_PORT/gd'																				
+
 	//url : 'process.env.OPENSHIFT_MONGODB_DB_URL'
 }
