95e9f132455437d93459147eaac37e34c7fb2cad
Toby <toby@skaffold.com>
2014-12-02 16:21:38 +1100

    first commit

---
+++ public/js/controllers/admin.js
@@ -0,0 +1,76 @@
+var app = angular.module('teamController', ['ngRoute']);
+app.controller('adminController', ['$scope','$http','Teams', '$location', function($scope, $http, Teams, $location) {
+
+		$scope.clearAll = function() {
+            Teams.clearAll()
+                .success(function(data) {
+                    $scope.loading = false;
+                    $scope.events = null;
+                    $scope.teams = null;
+                });
+        }
+        $scope.updateEvent = function(id) {
+            $scope.loading = true;
+            console.log('updateEvent '+id);
+            Teams.updateEvent(id)
+                .success(function(data) {
+                    $scope.loading = false;
+                    $scope.events = data; 
+                });
+        }
+		$scope.updateCompletedEvents = function() {
+            $scope.loading = true;
+            console.log('updateCompletedEvents');
+            Teams.updateCompletedEvents()
+                .success(function(data) {
+                    $scope.loading = false;
+                    $scope.events = data;
+                    $scope.funcCalled = "success";
+                });
+        }
+        $scope.processEvent = function(id) {
+            $scope.loading = true;
+            console.log('processEvent '+id);
+            Teams.processEvent(id)
+                .success(function(data) {
+                    $scope.loading = false;
+                    $scope.events = data; 
+                });
+        }
+		$scope.initEvents = function(id) {
+			$scope.loading = true;
+            console.log('initEvents '+id);
+			Teams.initEvents(id)
+				.success(function(data) {
+					$scope.loading = false;
+					$scope.events = data; 
+				});
+		};
+        $scope.initAllEvents = function() {
+            $scope.loading = true;
+            console.log('initAllEvents ');
+            Teams.initAllEvents()
+                .success(function(data) {
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.twitterSearch = function() {
+            $scope.loading = true;
+            console.log('twitterSearch ');
+            Teams.twitterSearch()
+                .success(function(data) {
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.funcCalled = null;
+        var initAction = ($location.search()).target;
+        console.log("initAction", initAction);
+        if (initAction == 'updateCompletedEvents') {
+            console.log("initAction-updateCompletedEvents");
+            $scope.updateCompletedEvents();
+            $scope.funcCalled = "loading";
+        }
+
+	}]);
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,24 +1,25 @@
 angular.module('teamController', [])
 
-	// inject the Team service factory into our controller
 	.controller('mainController', ['$scope','$http','Teams', function($scope, $http, Teams) {
-		$scope.formData = {};
+
 		$scope.loading = true;
 		$scope.eventCount = 10;
+        $scope.currentTeam = null;
+        $scope.currentRating = null;
+        $scope.currentRating = '';
+        $scope.ratings = ["A","B","C"];
 
 		$scope.init = function() {
 			$scope.loading = true;
 			console.log('Teams.init');
 			Teams.init()
-				// if successful creation, call our get function to get all the new teams
 				.success(function(data) {
 					console.log('Teams.init data res',data);
 				});
 		};
 
-		// GET =====================================================================
-		// when landing on the page, get all teams and show them
-		// use the service to get all the teams
+		// REST API ====
+		// when landing on the page get all teams
 		Teams.get()
 			.success(function(data) {
 				$scope.teams = data;
@@ -31,12 +32,6 @@ angular.module('teamController', [])
 				$scope.teams = data;
 				$scope.loading = false;
 			});
-
-//        Teams.getEvents()
-//            .success(function(data) {
-//                $scope.events = data;
-//                $scope.loading = false;
-//            });
 
         Teams.completedEvents()
             .success(function(data) {
@@ -59,49 +54,18 @@ angular.module('teamController', [])
             });
 
 
-
-		// CREATE ==================================================================
-		// when submitting the add form, send the text to the node API
-//		$scope.createTeam = function() {
-//
-//			// validate the formData to make sure that something is there
-//			// if form is empty, nothing will happen
-//			if ($scope.formData.text != undefined) {
-//				$scope.loading = true;
-//
-//				// call the create function from our service (returns a promise object)
-//				Teams.create($scope.formData)
-//
-//					// if successful creation, call our get function to get all the new teams
-//					.success(function(data) {
-//						$scope.loading = false;
-//						$scope.formData = {}; // clear the form so our user is ready to enter another
-//						$scope.teams = data; // assign our new list of teams
-//					});
-//			}
-//		};
-
-//		$scope.selectTeam = function(id) {
-//            $scope.currentTeam = id;
-//        }
-
-        $scope.currentTeam = null;
-        $scope.currentRating = null;
-        $scope.currentRating = '';
-        $scope.ratings = ["A","B","C"];
+        // local functions
 
         $scope.showMore = function() {
-            $scope.eventCount = $scope.eventCount + 5;
+            $scope.eventCount = $scope.eventCount + 13;
         }
 
         $scope.matchTeam = function(event){
-            //console.log('matchTeam',event);
             var res1 = $scope.matchTeamById(event);
             var res2 = $scope.matchTeamByRating(event);
             return res1 && res2;
         }
         $scope.matchTeamById = function(event){
-            //console.log('matchTeamById',event);
             if (!$scope.currentTeam || !$scope.currentTeam.team_id)
                 return true;
 
@@ -115,7 +79,6 @@ angular.module('teamController', [])
         }
 
         $scope.matchTeamByRating = function(event){
-            //console.log('matchTeamByRating',event);
             if (!$scope.currentRating)
                 return true;
 
@@ -124,69 +87,6 @@ angular.module('teamController', [])
 
             return false;
         }
-
-		$scope.clearAll = function() {
-            Teams.clearAll()
-                .success(function(data) {
-                    $scope.loading = false;
-                    $scope.events = null;
-                    $scope.teams = null;
-                });
-        }
-        $scope.updateEvent = function(id) {
-            $scope.loading = true;
-            console.log('updateEvent '+id);
-            Teams.updateEvent(id)
-                .success(function(data) {
-                    $scope.loading = false;
-                    $scope.events = data; // assign our new list of teams
-                });
-        }
-		$scope.updateCompletedEvents = function(id) {
-            $scope.loading = true;
-            console.log('updateCompletedEvents '+id);
-            Teams.updateCompletedEvents(id)
-                .success(function(data) {
-                    $scope.loading = false;
-                    $scope.events = data; // assign our new list of teams
-                });
-        }
-        $scope.processEvent = function(id) {
-            $scope.loading = true;
-            console.log('processEvent '+id);
-            Teams.processEvent(id)
-                .success(function(data) {
-                    $scope.loading = false;
-                    $scope.events = data; // assign our new list of teams
-                });
-        }
-		$scope.initEvents = function(id) {
-			$scope.loading = true;
-            console.log('initEvents '+id);
-			Teams.initEvents(id)
-				// if successful creation, call our get function to get all the new teams
-				.success(function(data) {
-					$scope.loading = false;
-					$scope.events = data; // assign our new list of teams
-				});
-		};
-        $scope.initAllEvents = function() {
-            $scope.loading = true;
-            console.log('initAllEvents ');
-            Teams.initAllEvents()
-                .success(function(data) {
-                    $scope.loading = false;
-                });
-        };
-
-        $scope.twitterSearch = function() {
-            $scope.loading = true;
-            console.log('twitterSearch ');
-            Teams.twitterSearch()
-                .success(function(data) {
-                    $scope.loading = false;
-                });
-        };
 
 
 
