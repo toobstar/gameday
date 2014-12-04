2ad724c64f875914142d992ff7a88b52b53922cf
Toby <toby@skaffold.com>
2014-12-04 11:50:47 +1100

    update db config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-	// the database url to connect																		
+    url : process.env.OPENSHIFT_MONGODB_DB_URL?
+                (process.env.OPENSHIFT_MONGODB_DB_URL + '/gd'):('mongodb://127.0.0.1:27017/gd')
 
-// mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu
 
-	url : process.env.OPENSHIFT_MONGODB_DB_HOST?
-		('mongodb://admin:_vx3Cb-zv6VC@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/gd'):
-		('mongodb://127.0.0.1:27017/gd')
-	//url : 'process.env.OPENSHIFT_MONGODB_DB_URL'
+//	url : process.env.OPENSHIFT_MONGODB_DB_HOST?
+//		('mongodb://admin:_vx3Cb-zv6VC@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/gd'):
+//		('mongodb://127.0.0.1:27017/gd')
+
 }
