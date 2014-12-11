2e6a7b0e151292eba9b2698d7226736f33ba351c
Toby <toby@skaffold.com>
2014-12-11 12:02:49 +1100

    - upcoming events display - improve timing around fetching updates - other general cleanup

--- public/js/services/teams.js
+++ public/js/services/teams.js
@@ -12,19 +12,6 @@ angular.module('teamService', [])
 				console.log('teamService initTeams');
 				return $http.get('/api/initTeams'+"/"+securityCode);
 			},
-//            updateEvent : function(id, securityCode) {
-//                console.log('teamService updateEvent');
-//                return $http.get('/api/updateEvent/'+id+"/"+securityCode);
-//            },
-//            processEvent : function(id, securityCode) {
-//                console.log('teamService processEvent');
-//                return $http.get('/api/processEvent/'+id+"/"+securityCode);
-//            },
-//            initEvents : function(id, securityCode) {
-//                console.log('teamService initEvents');
-//                return $http.get('/api/initEvents/'+id+"/"+securityCode);
-//            },
-
             upcomingEvents : function() {
                 return $http.get('/api/upcomingEvents');
             },
