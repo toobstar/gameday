30ae0da793e461b086ef17d0d9ff5473b30daa08
Toby <toby@skaffold.com>
2015-01-13 09:33:48 +1100

    - use function queue to clean up code - other small refactorings cleanups

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ var sprintf = require('sprintf').sprintf;
 var zlib = require('zlib');
 var _ = require("underscore");
 var Twit = require('twit');
-
+var FunctionQueue = require('functionqueue');
 
 if (process.env.TWITTER_CONS_KEY) {
     TWITTER_CONS_KEY = process.env.TWITTER_CONS_KEY;
@@ -31,15 +31,15 @@ else {
     process.exit(1);
 }
 
+var nbaQ = new FunctionQueue(5, 60, 5, "NBA-API"); // limit is 6 per minute.  5 gives a small buffer
+var twitterQ = new FunctionQueue(10, 60, 3, "TWITTER-API"); // limit is 450 per 15 min.  10 per minute is conservative.
+
 var T = new Twit({
     consumer_key:         TWITTER_CONS_KEY
     , consumer_secret:      TWITTER_CONS_SECRET
     , access_token:         TWITTER_ACCESS_TOKEN
     , access_token_secret:  TWITTER_ACCESS_SECRET
 })
-
-var apiDelayBetweenCalls = 11000; // 6 calls/minute max
-var apiCallInProgress = false;
 
 var processTeamResults = function(content) {
     console.log('print result ', content);
@@ -57,42 +57,32 @@ var processTeamResults = function(content) {
 
 function fetchTeamData() {
     var method = 'nba/teams';
-    fetchData(method,{},processTeamResults);
+    nbaQ.scheduleFn(fetchData, [method,{},processTeamResults]);
 }
 
 var processEventResults = function(content) {
-    apiCallInProgress = false;
     var events = JSON.parse(content);
     events.forEach(function (event) {
-        console.log('creating or updating: ',event);
+        //console.log('creating or updating: ',event);
         event.home_team_id=event.team.team_id;
         event.away_team_id=event.opponent.team_id;
         Event.findOneAndUpdate(
             {event_id: event.event_id},
             event,
             {upsert: true}, function(err, savedEvent){
-                console.log('Event.findOneAndUpdate result', err, savedEvent);
+                if (err) {
+                    console.log('Event.findOneAndUpdate result saved', err);
+                }
             });
     });
 }
 
-var teamIdQueue = [];
-function fetchTeamEvents() {
-    if (teamIdQueue.length > 0) {
-        if (apiCallInProgress == false) {
-            apiCallInProgress = true;
-            var teamId = teamIdQueue.shift();
-            var method = 'nba/results/'+teamId;
-            fetchData(method,{},processEventResults);
-        }
-
-        console.log('fetchTeamEvents waiting 11s - queued: ' + teamIdQueue.length);
-        setTimeout(function(){fetchTeamEvents()}, apiDelayBetweenCalls);
-    }
+function fetchTeamEvents(teamId) {
+    var method = 'nba/results/'+teamId;
+    nbaQ.scheduleFn(fetchData, [method,{},processEventResults]);
 }
 
 var processEventDetailResults = function(content, eventId) {
-    apiCallInProgress = false;
     console.log('processEventDetailResults ', eventId);
     var eventDetail = JSON.parse(content);
     console.log('creating or updating detail: ', eventId);
@@ -101,43 +91,12 @@ var processEventDetailResults = function(content, eventId) {
         {event_id: eventId},
         eventDetail,
         {upsert: true}, function(err, data){
-            console.log('Event.findOneAndUpdate (update) result', err)
+            if (err) {
+                console.log('Event.findOneAndUpdate (update) result', err)
+            }
             calcScores(data);
         });
 }
-
-var eventIdQueue = [];
-var apiPrevQueueLength = 0;
-var apiQueueUnchangedCount = 0;
-var queuedFetchEventDetail = function() {
-    console.log('queuedFetchEventDetail apiCallInProgress ',apiCallInProgress,eventIdQueue);
-    if (eventIdQueue.length > 0) {
-        if (apiCallInProgress == false) {
-            apiCallInProgress = true;
-            var eventId = eventIdQueue.shift();
-            console.log('queuedFetchEventDetail scheduled ',eventId);
-            fetchEventDetail(eventId);
-        }
-
-
-        // simple queue manager
-        if (apiPrevQueueLength == eventIdQueue.length && eventIdQueue.length > 0) {
-            apiQueueUnchangedCount++
-        }
-        else {
-            apiQueueUnchangedCount = 0;
-        }
-        if (apiQueueUnchangedCount > 10) {
-            console.log('resetting queue');
-            apiQueueUnchangedCount = 0;
-            apiCallInProgress = false;
-        }
-        apiPrevQueueLength = eventIdQueue.length;
-
-        console.log('queuedFetchEventDetail waiting 11s - queued: ' + eventIdQueue.length, apiCallInProgress);
-        setTimeout(function(){queuedFetchEventDetail()}, apiDelayBetweenCalls);
-    }
-};
 
 var lookup = {};
 lookup['atlanta-hawks']='#Hawks,#ATLHawks';
@@ -204,8 +163,6 @@ lookup2['utah-jazz']='UTA';
 lookup2['washington-wizards']='WAS';
 
 
-
-
 function fetchTwitterDetail(eventId) {
 
     Event.findOne(
@@ -215,8 +172,6 @@ function fetchTwitterDetail(eventId) {
             if (err) {
                 return "error"
             }
-
-            //var searchQuery = '#Warriors #OrlandoMagic  since:2014-11-26 until:2014-11-28';
 
             var awayTags = lookup[event.away_team_id];
             var homeTags = lookup[event.home_team_id];
@@ -242,7 +197,8 @@ function fetchTwitterDetail(eventId) {
               , include_entities: false
               , count: 100
             };
-            eventIdTwitterQueue.push([eventId, searchParams, 0]);
+
+            twitterQ.scheduleFn(askTwitter, [eventId, searchParams, 0]);
 
             /*
                 Second just match   #AWAY
@@ -259,7 +215,7 @@ function fetchTwitterDetail(eventId) {
                      , count: 100
                  };
 
-                 eventIdTwitterQueue.push([eventId, searchParams, 0]);
+                 twitterQ.scheduleFn(askTwitter, [eventId, searchParams, 0]);
             });
 
             /*
@@ -277,7 +233,7 @@ function fetchTwitterDetail(eventId) {
                      , count: 100
                  };
 
-                 eventIdTwitterQueue.push([eventId, searchParams, 0]);
+                 twitterQ.scheduleFn(askTwitter, [eventId, searchParams, 0]);
             });
 
             /*
@@ -296,53 +252,18 @@ function fetchTwitterDetail(eventId) {
                          , count: 100
                      };
 
-                     eventIdTwitterQueue.push([eventId, searchParams, 0]);
+                     twitterQ.scheduleFn(askTwitter, [eventId, searchParams, 0]);
                  });
             });
 
         });
 }
 
-var queueUnchangedCount = 0;
-var prevQueueLength = 0;
-var eventIdTwitterQueue = [];
-var processTwitterQueue = function() {
-  if (eventIdTwitterQueue.length > 0) {
-    if (apiCallInProgress == false) {
-      apiCallInProgress = true;
-      var twitQueue = eventIdTwitterQueue.shift();
-      var eventId = twitQueue[0];
-      var searchParams = twitQueue[1];
-      var iterationCount = twitQueue[2];
-      askTwitter(eventId, searchParams, iterationCount);
-    }
-
-      // delay: twitter rate limit is 450/15 min = (15*60)/450 <= 1 every 2 seconds
-    console.log('processTwitterQueue waiting 5s - queued: ' + eventIdTwitterQueue.length, queueUnchangedCount);
-
-    // simple queue manager
-    if (prevQueueLength == eventIdTwitterQueue.length && eventIdTwitterQueue.length > 0) {
-        queueUnchangedCount++
-    }
-    else {
-        queueUnchangedCount = 0;
-    }
-    if (queueUnchangedCount > 10) {
-        console.log('reseting queue');
-        queueUnchangedCount = 0;
-        apiCallInProgress = false;
-    }
-    prevQueueLength = eventIdTwitterQueue.length;
-    setTimeout(function(){processTwitterQueue()}, 5000);
-  }
-};
-
 function askTwitter(eventId, searchParams, iterationCount) {
     console.log(' ');
     console.log('');
     console.log('doing twitter search for query: ',searchParams.q, iterationCount);
     if (iterationCount > 15) {
-        apiCallInProgress = false;
         console.log('XXX reached max iteration count: ',searchParams.q, iterationCount);
         return;
     }
@@ -355,7 +276,6 @@ function askTwitter(eventId, searchParams, iterationCount) {
            }
 
           T.get('search/tweets', searchParams, function(err, data, response) {
-              apiCallInProgress = false;
               if (err) {
                   console.log('error with twitter search', searchParams, err);
               }
@@ -395,7 +315,7 @@ function askTwitter(eventId, searchParams, iterationCount) {
                   if (resultCount == 100 && data.search_metadata.max_id) {
                       searchParams['max_id'] = smallestId;
                       iterationCount++;
-                      eventIdTwitterQueue.push([eventId, searchParams, iterationCount]);
+                      twitterQ.scheduleFn(askTwitter, [eventId, searchParams, iterationCount]);
                   }
               }
           })
@@ -405,7 +325,7 @@ function askTwitter(eventId, searchParams, iterationCount) {
 function fetchEventDetail(eventId) {
     console.log('fetchEventDetail ', eventId);
     var method = 'nba/boxscore/'+eventId;
-    fetchData(method,{},processEventDetailResults, eventId);
+    nbaQ.scheduleFn(fetchData, [method,{},processEventDetailResults, eventId]);
 }
 
 function fetchData(method,params,resultProcessor, inputId) {
@@ -456,7 +376,6 @@ function fetchData(method,params,resultProcessor, inputId) {
             if (res.statusCode !== 200) {
                 // handle error...
                 console.warn("Server did not return a 200 response!\n" + chunks.join(''));
-                apiCallInProgress = false;
                 return;
                 //process.exit(1);
             }
@@ -467,7 +386,6 @@ function fetchData(method,params,resultProcessor, inputId) {
                     if (err) {
                         console.warn("Error trying to decompress data: " + err.message);
                         //process.exit(1);
-                        apiCallInProgress = false;
                         return;
                     }
                     resultProcessor(decoded, inputId);
@@ -479,7 +397,6 @@ function fetchData(method,params,resultProcessor, inputId) {
     }).on('error', function (err) {
             console.warn("Error trying to contact server: " + err.message);
             //process.exit(1);
-            apiCallInProgress = false;
             return;
         });
 }
@@ -511,8 +428,6 @@ function buildURL(host, sport, method, id, format, params) {
     }
     return url;
 }
-
-
 
 function getOpinions(req, res) {
     console.log('getOpinions');
@@ -672,7 +587,6 @@ function calcScores(event) {
             event.pointsBasedRating = 'C';
     }
 
-//    if (!event.aussies && event.fullModel && event.fullModel.away_stats && event.fullModel.home_stats) {
     if (event.fullModel && event.fullModel.away_stats && event.fullModel.home_stats) {
         event.aussies = [];
         var ausPlayers = ['EXUM','BAIRSTOW','BOGUT','PATTY','MILLS','INGLES','DELLAVEDOVA','MOTUM','BAYNES'];
@@ -718,9 +632,7 @@ function calcScores(event) {
         var eventDate = moment(event.event_start_date_time);
         if (eventDate.isBefore(now) && eventDate.isAfter(twitterSearchLimit)) {
             console.log('not yet loaded',event.event_id);
-            if (eventIdTwitterQueue.length < 20) {  // TODO limit while testing
-                fetchTwitterDetail(event.event_id);
-            }
+            fetchTwitterDetail(event.event_id);
         }
     }
 
@@ -814,14 +726,10 @@ module.exports = function (app) {
                     }
                     else {
                         console.log('not yet loaded',event.event_id);
-                        if (eventIdTwitterQueue.length < 20) {  // TODO limit while testing
-                            fetchTwitterDetail(event.event_id);
-                        }
+                        fetchTwitterDetail(event.event_id);
                     }
                 }
             });
-
-            setTimeout(function(){processTwitterQueue()}, 2000);
 
             res.send('done')
         });
@@ -880,10 +788,9 @@ module.exports = function (app) {
 
             _.each(teams,function(team){
                 console.log('initEvents for '+team.team_id);
-                teamIdQueue.push(team.team_id);
+                fetchTeamEvents(team.team_id);
             });
 
-            fetchTeamEvents();
         });
 
         res.send('done')
@@ -909,25 +816,9 @@ module.exports = function (app) {
 
             _.each(events,function(event){
 
-                //var now = moment().subtract(10, 'hours');
-                //var eventDate = moment(event.event_start_date_time);
                 var eventStartDateETzone = moment(event.event_start_date_time).zone("-04:00");
                 var eventFinishDateETzone = eventStartDateETzone.clone().add(2, 'hours');
                 var nowETzone = moment().zone("-04:00");
-
-                if (event.event_id.indexOf('20141210-boston-') > -1) {
-                    //console.log('1)checking whether event happened yet.  eventDate: '+eventDate + " moment: " + moment() + " now (sub10): "+ now);
-                    //console.log('2)checking whether event happened yet.  eventDate: '+event.event_start_date_time + " now: " + (new Date()));
-                    //console.log('3)checking whether event happened yet.  eventDate: '+eventDate.format("dddd, MMMM Do YYYY, h:mm:ss a") + " now: " + nowETzone.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-                    console.log('4)checking whether event happened yet.  eventStartDateETzone: '+eventStartDateETzone.format("dddd, MMMM Do YYYY, h:mm:ss a") +
-                        " eventFinishDateETzone: " + eventFinishDateETzone.format("dddd, MMMM Do YYYY, h:mm:ss a") +
-                        " now: " + nowETzone.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-
-//                    xxxx  20141210-boston-celtics-at-charlotte-hornets
-//                    2)checking whether event happened yet.  eventDate: 2014-12-10T19:00:00-05:00 now: Thu Dec 11 2014 10:46:27 GMT+1100 (AEDT)
-//                    3)checking whether event happened yet.  eventDate: Thursday, December 11th 2014, 11:00:00 am now: Wednesday, December 10th 2014, 7:46:27 pm
-//                    4)checking whether event happened yet.  eventDate: Wednesday, December 10th 2014, 8:00:00 pm now: Wednesday, December 10th 2014, 7:46:27 pm
-                }
 
                 if (eventFinishDateETzone.isAfter(nowETzone)) {
                     console.log('after now ',eventFinishDateETzone.format("dddd, MMMM Do YYYY, h:mm:ss a"));
@@ -940,41 +831,15 @@ module.exports = function (app) {
                     }
                     else {
                         console.log('XXXX not yet set box score',event.event_start_date_time);
-                        eventIdQueue.push(event.event_id);
+                        fetchEventDetail(event.event_id);
                     }
                 }
                 //console.log('boxScoreForCompleted ',event.event_id, event.event_start_date_time);
             });
 
-            queuedFetchEventDetail();
-            setTimeout(function(){processTwitterQueue()}, 10000);
-
             res.send('done')
         });
     });
-
-//    app.get('/api/processEvent/:event_id/:securityCode', function (req, res) {
-//        if (SECURITY_CODE !== req.params.securityCode) {
-//            console.log("invalid securityCode ", req.params.securityCode);
-//            res.send('invalid');
-//            return;
-//        }
-//
-//        console.log('processEvent for '+req.params.event_id, req.params.securityCode);
-//
-//        Event.findOne(
-//            {event_id: req.params.event_id},
-//            function(err, event){
-//                console.log('Event.findOne result', err, event)
-//                if (err) {
-//                    return "error"
-//                }
-//                calcScores(event);
-//            });
-//
-//
-//        getEvents(res);
-//    });
 
     app.get('/api/flagEvent/:event_id/:status', function (req, res) {
 
