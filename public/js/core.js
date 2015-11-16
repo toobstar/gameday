0a96f85279660abae85f6fd09fbcae15b3a7f7fc
toby <toby@skaffold.com>
2015-11-16 16:07:26 +1100

    click to load game- added some route stuff

--- public/js/core.js
+++ public/js/core.js
@@ -1,2 +1,16 @@
 //angular.module('gameDay', ['mainController', 'teamService']);
-var app = angular.module( 'gameDay', [] );
+angular.module( 'gameDay', ['mainController', 'teamService'] );
+
+
+
+//angular.module('skaffoldApp', ['ngRoute',,'ngSanitize'])
+//    .config(['$routeProvider', function ($routeProvider) {
+//        $.each(routes, function (route, url) {
+//            $routeProvider.when(route, {templateUrl: url});
+//        });
+//
+//        if(defaultRoute)
+//            $routeProvider.otherwise({redirectTo: defaultRoute});
+//        else
+//            $routeProvider.otherwise({redirectTo: '/'});
+//    }])
